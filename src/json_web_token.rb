40a9c7c2f4b9a42b2010907429b6168cf946e4f9
kura-lab <kmasaru86@gmail.com>
2014-11-27 02:26:19 +0900

    added encode method

--- src/json_web_token.rb
+++ src/json_web_token.rb
@@ -1,9 +1,24 @@
-# JSON Web Token Class
-class JsonWebToken
-  def initialize
-  end
+require 'base64'
+require 'json'
+require 'openssl'
 
-  def encode
-    ''
+#
+# JSON Web Token Class
+#
+class JsonWebToken
+  def encode(header, payload, secret)
+    h = Base64.urlsafe_encode64(JSON.generate(header)).gsub('=', '')
+    p = Base64.urlsafe_encode64(JSON.generate(payload)).gsub('=', '')
+    if header[:alg] == 'HS256'
+      ssl = OpenSSL::Digest::SHA256.new
+    elsif header[:alg] == 'HS384'
+      ssl = OpenSSL::Digest::SHA384.new
+    elsif header[:alg] == 'HS512'
+      ssl = OpenSSL::Digest::SHA512.new
+    else
+      fail
+    end
+    sig = OpenSSL::HMAC.hexdigest(ssl, secret, h + p)
+    [h, p, sig].join('.')
   end
 end
