f6e573358184f316bc07e0dbd8893db89ce74c6a
kura-lab <kmasaru86@gmail.com>
2014-11-28 02:45:35 +0900

    updated

--- spec/json_web_token_spec.rb
+++ spec/json_web_token_spec.rb
@@ -2,22 +2,39 @@ require 'spec_helper'
 require './src/json_web_token'
 
 describe JsonWebToken do
+  before do
+    @jwt = JsonWebToken.new
+    @issued_jwt = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL2V4YW1wbGUuY29tIiwiYXVkIjoiY2xpZW50MTIzIiwic3ViIjoidXNlcjEyMyIsImV4cCI6MTQxNzEwNzc5MiwiaWF0IjoxNDE3MDIxMzkyLCJub25jZSI6ImFhYWJiYmNjYyJ9.YTNiZTFmMzczM2E2OTBhOWZiNzM1MTliYmIzMzU4MzI0YmM1ZDIzNWNhYmE4NjViZTM1NmIxNDE1ODQwM2FkNg'
+    @secret = 'xxxyyyzzz'
+    @time_stamp = 1417021392
+  end
+
   it 'is encoded jwt' do
     header = {
       alg: 'HS256',
       typ: 'JWT'
     }
-    time_stamp = 1417021392
     payload = {
       iss:   'https://example.com',
       aud:   'client123',
-      exp:   time_stamp + 24 * 60 * 60,
-      iat:   time_stamp,
+      sub:   'user123',
+      exp:   @time_stamp + 24 * 60 * 60,
+      iat:   @time_stamp,
       nonce: 'aaabbbccc'
     }
-    secret = 'xxxyyyzzz'
-    jwt = JsonWebToken.new
-    enc = jwt.encode(header, payload, secret)
-    expect(enc).to eq 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL2V4YW1wbGUuY29tIiwiYXVkIjoiY2xpZW50MTIzIiwiZXhwIjoxNDE3MTA3NzkyLCJpYXQiOjE0MTcwMjEzOTIsIm5vbmNlIjoiYWFhYmJiY2NjIn0.2dad1d996991292badcfbaa80097b89efc17c7ba7a2fff6c8819b0f33a00e55f'
+    enc = @jwt.encode(header, payload, @secret)
+    expect(enc).to eq @issued_jwt
+  end
+
+  it 'is decoded jwt' do
+    header, payload = @jwt.decode(@issued_jwt, @secret)
+    expect(header['alg']).to eq 'HS256'
+    expect(header['typ']).to eq 'JWT'
+    expect(payload['iss']).to eq 'https://example.com'
+    expect(payload['aud']).to eq 'client123'
+    expect(payload['sub']).to eq 'user123'
+    expect(payload['exp']).to eq @time_stamp + 24 * 60 * 60
+    expect(payload['iat']).to eq @time_stamp
+    expect(payload['nonce']).to eq 'aaabbbccc'
   end
 end
